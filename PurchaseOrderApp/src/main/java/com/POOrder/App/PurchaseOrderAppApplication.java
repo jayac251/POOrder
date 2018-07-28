2fe2fe70f9719e2d2d4368ad676c0b9e7eaf55ad
Chitra <Chitra@DESKTOP-1BHEOKR>
2018-07-28 11:25:04 -0700

    InitialCOmmit

---
+++ PurchaseOrderApp/src/main/java/com/POOrder/App/PurchaseOrderAppApplication.java
@@ -0,0 +1,12 @@
+package com.POOrder.App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PurchaseOrderAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PurchaseOrderAppApplication.class, args);
+	}
+}
